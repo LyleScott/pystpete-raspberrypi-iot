51e84f5f03cbbf806378aa5bdc8e27b6b2caf576
Lyle Scott, III <lyle@digitalfoo.net>
2016-09-26 23:57:43 -0400

    add unicornhat scroller example

---
+++ unicornhat_scroller.py
@@ -0,0 +1,16 @@
+"""
+Lyle Scott, III
+lyle@digitalfoo.net
+
+pip install unicornhat
+git clone https://github.com/topshed/UnicornHatScroll
+<put this script in the UnicornHatScroll to run it.
+(or put that directory into PYTHONPATH)
+"""
+from UHScroll import *
+import time
+
+unicorn_scroll('Py', 'blue', 255, 0.08)
+unicorn_scroll('St', 'white', 150, 0.08)
+unicorn_scroll('Pete', 'red', 200, 0.08)
+unicorn_scroll('~smile ~smile', 'green', 200, 0.1)
